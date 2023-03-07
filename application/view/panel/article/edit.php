5a4b5b0d3356fdcd7f22991519a580848c94f690
Nima8FT <Nima_8ak@yahoo.com>
2023-03-07 18:53:31 +0330

    finishing index & create & update in folder article

--- application/view/panel/article/edit.php
+++ application/view/panel/article/edit.php
@@ -1,30 +1,25 @@
 <?php $this->include('panel.layouts.header') ?>
 
-<form>
+<form action="<?php $this->url('article/store/' . $article['id']) ?>" method="POST">
   <section class="form-group">
     <label for="title">Title</label>
-    <input
-      type="text"
-      class="form-control"
-      id="title"
-      placeholder="title ..."
-    />
+    <input type="text" class="form-control" name="title" value="<?= $article['title'] ?>" id="title"
+      placeholder="title ..." />
   </section>
   <section class="form-group">
     <label for="cat_id">Category</label>
-    <select class="form-control" id="cat_id">
-      <option value="1">Sport</option>
-      <option value="2">News</option>
+    <select class="form-control" id="cat_id" name="cat_id">
+      <?php foreach ($categories as $category) {
+        if ($article['cat_id'] == $category['id']) { ?>
+          <option value="<?= $category['id'] ?>"><?= $category['name'] ?></option>
+        <?php }
+      } ?>
     </select>
   </section>
   <section class="form-group">
     <label for="body">Body</label>
-    <textarea
-      class="form-control"
-      id="body"
-      rows="5"
-      placeholder="body ..."
-    ></textarea>
+    <textarea class="form-control" value="<?= $article['body'] ?>" id="body" name="body" rows="5"
+      placeholder="body ..."></textarea>
   </section>
   <button type="submit" class="btn btn-primary">Update</button>
 </form>
